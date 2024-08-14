aa1145319ff6e06e62ddb53069ace8389ce7d956
ab-h-i-n <abhinshelly@gmail.com>
2024-08-14 11:23:59 +0530

    styling home page

--- welcome/index.php
+++ welcome/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,18 @@
     <script src="../scripts/login-signup.js" defer></script>
     <title>Document</title>
 </head>
+
 <body>
-    <?php include('../common/header.php') ?>
+    <?php include ('../common/header.php') ?>
+    <section class="home-section">
+        <div class="title">
+            UNLOCK YOUR CODING POTENTIAL
+        </div>
+
+        <div class="hero-image">
+            <img src="../public/images/hero-image.svg" alt="hero-image">
+        </div>
+    </section>
 </body>
+
 </html>
