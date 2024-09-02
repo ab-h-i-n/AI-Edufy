ff12309f05134d90d83aaf0ead1a6de33688fafe
ab-h-i-n <abhinshelly@gmail.com>
2024-09-02 15:16:38 +0530

    mentor page

--- common/questionBox.php
+++ common/questionBox.php
@@ -1,35 +1,108 @@
 <head>
     <style>
-        .ques_header{
+
+        .question-box{
+            background-color: #272729;
+            padding: 2.3rem;
+            border-radius: 10px;
+            border: var(--default-border);
+            cursor: pointer;
+            transition: all .5s;
+
+            &:hover{
+                background-color: #444447;
+            }
+        }
+
+        .ques_header {
             display: flex;
             justify-content: space-between;
 
-            & .tags{
-            display: flex;
-            gap: .5rem;
+            & .title {
+                font-size: 1.3rem;
+                font-weight: 600;
+
+                & .points{
+                    font-size: .8rem;
+                    font-weight: 900;
+                    background-color: lightgoldenrodyellow;
+                    color: #272729;
+                    padding: .3rem .5rem;
+                    border-radius: 10px;
+                    margin-left: .5rem;
+                }
+            }
+
+            & .sub-title {
+                font-size: .8rem;
+                font-weight: 600;
+                opacity: .5;
+            }
+
+            & .tags {
+                display: flex;
+                gap: .5rem;
+
+                & .pill{
+                    background-color: gray;
+                    border: var(--default-border);
+                    border-radius: 20px;
+                    height: min-content;
+                    padding: .3rem .7rem;
+                    text-transform: capitalize;
+                }
+
+                & .easy{
+                    background-color: green;
+                }
+
+                & .medium{
+                    background-color: orangered;
+                }
+
+                & .hard{
+                    background-color: red;
+                }
+
+                & .cpp{
+                    background-color: violet;
+                }
+
+                & .c{
+                    background-color: blue;
+                }
+            }
         }
+
+        .ques-desc{
+            margin-top: 1rem;
+            font-size: 1.2rem;
+            opacity: .7;
         }
     </style>
 </head>
-<div>
+<div class="question-box">
     <div class="ques_header">
 
         <div>
             <!-- title  -->
-             <p><?php echo $question['title']; ?></p>
-             <!-- by  -->
-              <p>by mentor name</p>
+            <p class="title">
+                <?php echo $question['title']; ?>
+                <span class="points"> <?php echo $question['points']; ?>Pts </span>
+            </p>
+            <!-- by  -->
+            <p class="sub-title">by mentor name</p>
         </div>
 
         <!-- tags  -->
         <div class="tags">
             <!-- type  -->
-             <p>Easy</p>
-             <!-- language   -->
-              <p>C++</p>
+            <p class="type pill <?php echo $question['type']; ?>" ><?php echo $question['type']; ?></p>
+            <!-- language   -->
+            <p class="lang pill <?php $lang = ($question['language'] == "c++") ? "cpp" : $question['language']; echo $lang; ?>"><?php echo $question['language']; ?></p>
         </div>
 
     </div>
     <!-- question description  -->
-    <p><?php echo $question['description']; ?></p>
+    <p class="ques-desc"><?php echo $question['description']; ?></p>
 </div>
--- users/mentor/index.php
+++ users/mentor/index.php
@@ -1,9 +1,10 @@
 <?php
-    include_once('../utils/connect.php');
-    $userId = $_COOKIE['user_id'];
-    $userQuestions = $user->getUserQuestions($userId);
+include_once('../utils/connect.php');
+$userId = $_COOKIE['user_id'];
+$userQuestions = $user->getUserQuestions($userId);
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,28 +12,40 @@
     <link rel="stylesheet" href="../styles/mentor-page.css">
     <title>Document</title>
 </head>
+
 <body id="mentor">
     <?php include('../common/header.php') ?>
 
     <main>
 
         <!-- questions section  -->
-         <section class="question-section">
+        <section class="question-section">
             <!-- title  -->
-             <p class="title">My Questions</p>
-             <!-- quesitons  -->
-              <div class="questions-container">
-                <?php foreach ($userQuestions as $question){
-                    include('../common/questionBox.php');
+            <p class="title">My Questions</p>
+            <!-- quesitons  -->
+            <div class="questions-container">
+                <?php
+                    if (isset($userQuestions)) {
+
+                        foreach ($userQuestions as $question) {
+                            include('../common/questionBox.php');
+                        }
+                    } else {
+                ?>
+                
+                <div>No Questions</div>
+                
+                <?php
                 }
                 ?>
-              </div>
-         </section>
+            </div>
+        </section>
 
         <!-- side section  -->
-         <section class="side-section"></section>
+        <section class="side-section"></section>
 
     </main>
 
 </body>
+
 </html>
